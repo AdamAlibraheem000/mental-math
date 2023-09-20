4a49d2f4e097e08806fc7b662bf62b54b8f9c5ab
adamalibraheem000 <adamalibraheem@hotmail.com>
2023-09-20 08:57:51 -0400

    initial

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+int main()
+{
+    cout << "Hello world\n";
+    
+    
+    return 0;
+}
